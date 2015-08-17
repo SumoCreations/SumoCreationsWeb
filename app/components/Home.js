a6efcb3bce95fd605a48cd31b959da1a08d62b0e
Jim Jeffers <jim@sumocreations.com>
2015-08-17 12:32:04 -0700

    Implemented a complete SCSS build sample.

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,7 +3,13 @@ import React from 'react';
 export default class Home extends React.Component {
   render() {
     return (
+      <div className="home">
         <h2>Welcome Home!</h2>
+        <a className="button" href="#">Anchor button</a>
+        <button>Button element</button>
+        <input type="submit" value="submit input" />
+        <input type="button" value="button input" />
+      </div>
     );
   }
 };
